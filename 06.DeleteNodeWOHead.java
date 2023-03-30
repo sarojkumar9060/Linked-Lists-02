db2b052e5e512128e5243a67f98bb5d4d5531659
Er saroj Kumar Roy <79388784+sarojkumar9060@users.noreply.github.com>
2023-03-31 00:05:35 +0530

    Add files via upload

---
+++ 06.DeleteNodeWOHead.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+// LC 237: Time = O(1), Space = O(1)
+
+// Definition for singly-linked list
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        // Store Data of Next Node in Node
+        node.val = node.next.val;
+
+        // Delete Next Node
+        node.next = node.next.next;
+    }
+}
+
